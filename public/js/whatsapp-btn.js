609342015f283bf76835fea1a7992ea6200eb388
Kaldo <dantekaled100@gmail.com>
2023-07-04 12:23:32 -0600

    feat: WP-44 Hacer funcionar el boton de Whatsapp

--- public/js/whatsapp-btn.js
+++ public/js/whatsapp-btn.js
@@ -1,8 +1,8 @@
 const whatsappBtn = document.getElementById('whatsapp-btn');
 
 whatsappBtn.addEventListener('click', () => {
-    const phoneNumber = '8712174679'; // Reemplaza esto con tu número de teléfono
-    const message = '¡Escribe aquí como podemos ayudarte!'; // Reemplaza esto con el mensaje que quieras enviar
+    const phoneNumber = '+528712646982'; // Reemplaza esto con tu número de teléfono
+    const message = '¡Me gustaria pedir informes!'; // Reemplaza esto con el mensaje que quieras enviar
 
     const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
         message
